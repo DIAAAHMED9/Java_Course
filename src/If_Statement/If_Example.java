7923747e121328390f0e4aca2e6e3c2d298eaf5e
Diaa <adksj@gmail.com>
2023-01-21 13:49:22 +0200

    Course

---
+++ src/If_Statement/If_Example.java
@@ -0,0 +1,140 @@
+package If_Statement;
+
+import java.util.Scanner;
+
+public class If_Example {
+    public static void main(String[] args) {
+
+//        Exanple #3
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the First Number");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter the operation");
+        String ope = scanner.next();
+        System.out.println("Enter the Second Number");
+        int num2 = scanner.nextInt();
+        int result = 0;
+        if (ope.equals("*")) {
+            result = num1 * num2;
+            System.out.println("The result is " + result);
+
+            if (6 < 9) {
+                System.out.println("correct");
+            }
+        } else if (ope.equals("+")) {
+            result = num1 + num2;
+        } else if (ope.equals("-")) {
+            result = num1 - num2;
+        } else if (ope.equals("/")) {
+            result = num1 / num2;
+        } else {
+            System.out.println("Error");
+        }
+        System.out.println("The result is " + result);
+
+
+//        Example #2
+//        System.out.println("Enter your number");
+//        int mark = scanner.nextInt();
+//        if (mark <100 && mark >=90){
+//            System.out.println("A");
+//        } else if (mark < 90 && mark >=80) {
+//            System.out.println("B");
+//        }else if (mark <80 && mark>=70) {
+//            System.out.println("C");
+//        }else {
+//            System.out.println("F");
+//        }
+
+//        Example #1
+//        if (num1%2 ==0){
+//            System.out.println("even number");
+//        }else{
+//            System.out.println("odd number");
+//        }
+
+//   Short   If
+//        variable = (condition) ? expressionTrue :  expressionFalse;
+
+//        String test =(num1%2 ==0) ?("even number"):("odd number");
+//
+//        System.out.println(test);
+
+
+//
+//        //4  nested if statement
+//        if (condition1) {
+//            // codes
+//            if (condition1) {
+//                // codes
+//            } else if (condition2) {
+//                // codes
+//            } else if (condition3) {
+//                // codes
+//            } else {
+//                // codes
+//            }
+//        } else if (condition2) {
+//            // codes
+//        } else if (condition3) {
+//            // codes
+//        } else {
+//            // codes
+//        }
+//
+////        3  if-else-if ladder
+//        if (condition1) {
+//            // codes
+//        } else if (condition2) {
+//            // codes
+//        } else if (condition3) {
+//            // codes
+//        } else {
+//            // codes
+//        }
+//
+//
+//        //2 if-else statement
+//        if (condition1) {
+//            // codes
+//        } else {
+//            // codes
+//        }
+//
+////        1  if statement
+//        if (condition1) {
+//            // codes
+//        }
+
+
+//
+//        int x = 5, y = 5;
+//        if (x < y) {
+//            System.out.println("القيم  متساوية  ");
+//            if (9 != 9) {
+//                System.out.println("true");
+//            } else if (8 == 9) {
+//                System.out.println("correct");
+//            } else {
+//                System.out.println("false");
+//            }
+//        } else if (x < y) {
+//            System.out.println("x اكبر منyقيمة  ");
+//
+//        } else if (x < y) {
+//            System.out.println("القيم متساوية او y اكبر من x");
+//        } else if (x > y) {
+//            System.out.println("القيم متساوية او x اكبر من y");
+//        } else if (x != y) {
+//            System.out.println("القيم غير متساوية  ");
+//        } else if (x > y) {
+//            System.out.println(" y اكبر من xقيمة  ");
+//        } else {
+//            System.out.println("احد الفيم التي تم اضافتها خاطئة");
+//        }
+//
+//
+//        System.out.println("End if");
+
+    }
+}
---
+++ src/Introduction_In_Java/String_Number.java
@@ -0,0 +1,160 @@
+package Introduction_In_Java;
+
+public class String_Number {
+    public static void main(String[] args) {
+
+//
+//        int num1=10, num2=5;
+//        num1 =num1+num2;
+//        System.out.println(num1);
+//        num2 = num1 -num2;
+//        System.out.println(num2);
+//        num1 =num1-num2;
+//        System.out.println("Number 1 is "+num1 +"Number 2 is "+ num2);
+
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter First Number ");
+//        int num1 = scanner.nextInt();
+//        System.out.println("Enter Second Number ");
+//        int num2 = scanner.nextInt();
+//        int sum = num1 + num2;
+//        int diff = num1 - num2;
+//        int pow = num1 * num2;
+//        double aver = (double) (num1 + num2) / 2;
+//
+//
+//        System.out.println(sum);
+//        System.out.println(diff);
+//        System.out.println(Math.abs(diff));
+//        System.out.println(pow);
+//        System.out.println(aver);
+//        System.out.println(Math.max(num1, num2));
+//        System.out.println(Math.min(num1, num2));
+
+
+//        System.out.println("Enter Fist Name");
+//        String name = scanner.nextLine();
+//
+//        System.out.println("Enter Last Name");
+//        String last_name = scanner.nextLine();
+//        String full_nama = name + last_name;
+//        System.out.println(full_nama);
+//        System.out.println(full_nama.toUpperCase());
+//        System.out.println(full_nama.toLowerCase());
+//        System.out.println(name.charAt(0) + "   " + last_name.charAt(0));
+
+
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter your name ");
+//        String name = scanner.nextLine();
+//        System.out.println("Enter your Age");
+//        int age = scanner.nextInt();
+//        System.out.println("your name is " + name + "\nage is " + age);
+//
+
+
+//        boolean true = true;
+//        boolean fal= false;
+//        System.out.println(tr);
+//        System.out.println(fal);
+
+//
+//        int x= 6;
+//        int y = 6;
+//        System.out.println(!(x > 5 ||  x >10));
+
+
+//        byte num = 17;
+//        short num3 = 6853;
+//        int num1 = 287998798;
+//        long num5 = 68973457777863478l;
+//
+//        float num45 = 76786324875678642376783246687326786766.0f;
+//        double num54 = 76786324875678642376783287676436.0;
+//
+
+
+//        تحويل من الصغير الى الكبير يتم تلقائيا
+//        int num = 3;
+//        short num_casting =(short) num;
+//
+//        System.out.println(num);
+////        System.out.println(num_casting);
+
+
+//        تحويل من الكبير الى الصغير يجب القيام بالعمليات التالية
+
+//
+//        long num2 = 9;
+//        int num_casting2 = (int) num2;
+//        System.out.println(num2);
+//        System.out.println(num_casting2);
+//
+//
+//
+//
+//
+//
+//
+//        String test = "Hello Man";
+//        String test2 = " Welcome";
+//        System.out.println(test.length());
+//        System.out.println(test.toUpperCase());
+//        System.out.println(test.toLowerCase());
+//        System.out.println(test.indexOf("Man"));
+//
+//        System.out.println(test +" "+test2);
+//
+//        System.out.println(test.concat(test2));
+//
+//
+//        System.out.println("it\'s");
+//        System.out.println("\"casting\" is ");
+
+
+//        int num1 = 6;
+//        int num2 = 8;
+//        int num3 = (int) (Math.random() * 51);
+//        System.out.println(num3);
+//        System.out.println(Math.abs(-9));
+//        System.out.println(Math.sqrt(7));
+
+
+//
+//        int num = 6;
+//
+//        System.out.println(num);
+//
+//
+//        int num1 = 6;
+//        num1 = num1 + 5;
+//        System.out.println(num1);
+//        int num2 = 4;
+//        System.out.println(6%4);
+//        System.out.println(num1--); //num1 = 5
+//        System.out.println(--num1);// num1 = 8
+
+//
+//
+//        String name = "Diaa";
+//        int age = 3;
+//
+//        String id = name + age;
+//        System.out.println(id);
+//        System.out.println(name + age );
+
+// name old data type
+//        int z = 3, a = 2, e = name old data type 1;
+//
+//        final int num544 = 5;
+//        System.out.println(num544);
+
+//        int z =3;
+
+//        int ;
+
+
+    }
+}
