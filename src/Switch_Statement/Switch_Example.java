fac5d12ba5dcc2083e3f79b20c7004b12aefd403
Diaa <adksj@gmail.com>
2023-01-22 12:35:15 +0200

    Course

--- src/Switch_Statement/Switch_Example.java
+++ src/Switch_Statement/Switch_Example.java
@@ -1,5 +1,35 @@
 package Switch_Statement;
 
-public class Switch_Example {
+import java.util.Scanner;
 
+public class Switch_Example {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the First Number");
+        double num1 = scanner.nextInt();
+        System.out.println("Enter the operation");
+        char ope = scanner.next().charAt(0);
+        System.out.println("Enter the Second Number");
+        double num2 = scanner.nextInt();
+        double result = 0;
+        switch (ope) {
+            case '*':
+                result = num1 * num2;
+                break;
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Error");
+
+        }
+        System.out.println("The result is " + result);
+    }
 }
