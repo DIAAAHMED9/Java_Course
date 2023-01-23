8430fc58c4ae6a6cc58ae600f71f17d71fbbc5a1
Diaa <adksj@gmail.com>
2023-01-24 01:35:53 +0200

    Course

--- src/Loop/For_Loop.java
+++ src/Loop/For_Loop.java
@@ -1,4 +1,24 @@
 package Loop;
 
 public class For_Loop {
+    public static void main(String[] args) {
+        for (int x = 1; x <= 5; x++) {
+
+            for (int y = 1; y <= 5; y++) {
+
+                System.out.println("X =" + x + " " + "Y =" + y);
+
+            } //end y
+        }// end x
+
+
+//        Example
+        int star = 5;
+        for (int i = 1; i <= star; i++) {
+            for (int j = star; j >= i; j--) {
+                System.out.print("+");
+            } //end j
+            System.out.println();
+        } //end i
+    }
 }
